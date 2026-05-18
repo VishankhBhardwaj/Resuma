59bb6c9bbae9e7c6dd53a2b5ddd3561217aec8c6
Vishankh <pushkar.vikas8@yahoo.com>
2026-05-18 17:05:51 +0530

    Delete src/app/voice-preview directory

--- src/app/voice-preview/page.js
+++
@@ -1,9 +0,0 @@
-import VoiceMockInterviewUI from "../../../voice-interview-ui/VoiceMockInterviewUI";
-
-export const metadata = {
-    title: "Voice Interview Preview",
-};
-
-export default function VoicePreviewPage() {
-    return <VoiceMockInterviewUI />;
-}
